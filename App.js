f0b445763fe080cc7a4f1199e3ab2d8b3950d3d4
Macbook Air <ma@Macbooks-MacBook-Air.local>
2022-05-15 00:16:45 +0630

    start gs project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const App = props => {
+  return(
+      <View>
+        <Text>Hello GSMyanmar</Text>
+      </View>
+  )
+}
+
+
+export default App
